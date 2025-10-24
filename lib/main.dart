7deda7850502de4fc41aa73be548ce9d72469b50
kidev14 <adekatuasa2001@gmail.com>
2025-10-24 11:32:01 +0700

    buat title supaya ga localhost

--- lib/main.dart
+++ lib/main.dart
@@ -17,39 +17,3 @@ Widget build(BuildContext context ) {
   );
 }
 }
-// pages
-class Halaman extends StatelessWidget {
-  const Halaman({super.key});
-
-  @override
-  Widget build(BuildContext context ) {
-    return Scaffold(
-      appBar: AppBar(
-      backgroundColor: Colors.amberAccent ,
-    title: Text('Halaman Depan'),
-
-    actions: [
-      Icon(Icons.person),
-      SizedBox(width: 10),
-      Icon(Icons.account_balance),
-      SizedBox(width: 5),
-    ],
-  ),
-  body: Container(
-        width: double.infinity,
-        height: 100,
-        color: Colors.blue,
-        alignment: Alignment.topCenter,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          Icon(Icons.person),
-          SizedBox(width: 10),
-          Icon(Icons.account_balance),
-          SizedBox(width: 5),
-          Text("ini adalah data"),
-       ],)
-     ),  
-    );
-  }
-}
