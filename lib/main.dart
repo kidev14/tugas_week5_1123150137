d8a87ad1283d97118a45eb399b68ca16166bbeab
kidev14 <adekatuasa2001@gmail.com>
2025-10-27 23:23:43 +0700

    menambahkan nama

--- lib/main.dart
+++ lib/main.dart
@@ -1,55 +1,109 @@
 import 'package:flutter/material.dart';
 
-void main () {
-  runApp(const MyApp());
-
+void main() {
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp ({super.key});
+  const MyApp({super.key});
 
-@override
-Widget build(BuildContext context ) {
-  return MaterialApp(
-    title: "latihan container",
-    debugShowCheckedModeBanner: false,
-    home: Halaman(),
-  );
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Latihan Container",
+      debugShowCheckedModeBanner: false,
+      home: Halaman(),
+    );
+  }
 }
-}
-// pages
+
 class Halaman extends StatelessWidget {
   const Halaman({super.key});
 
   @override
-  Widget build(BuildContext context ) {
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-      backgroundColor: Colors.amberAccent ,
-    title: Text('Halaman Depan'),
+        backgroundColor: Colors.deepOrange,
+        title: Text("Halaman Depan"),
+        actions: [
+          Icon(Icons.add_a_photo),
+          SizedBox(width: 10),
+          Icon( Icons.access_alarm),
+          SizedBox(width: 10),
+          Icon(Icons.search),
+          SizedBox(width: 10),
+          Icon(Icons.account_circle),
+          SizedBox(width: 10),
+        ],
+        flexibleSpace: Container(
+        decoration: const BoxDecoration(
+        gradient: LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Colors.deepOrange, // warna atas
+          Colors.white,      // warna bawah
+        ],
+      ),
+    ),
+      )
+      ),
+      body: Container(
+        margin: EdgeInsets.all(5),
+        // width: double.infinity,
+        // height: double.infinity,
+        // color: Colors.blueGrey,
+        alignment: Alignment.center,
 
-    actions: [
-      Icon(Icons.person),
-      SizedBox(width: 10),
-      Icon(Icons.account_balance),
-      SizedBox(width: 5),
+        // child: Row(
+        //   mainAxisAlignment: MainAxisAlignment.center,
+        //   children: [
+        //     Icon(Icons.home_repair_service),
+        //     SizedBox(width: 10),
+        //     Icon(Icons.account_balance),
+        //     SizedBox(width: 10),
+        //     Text("Ini adalah data")
+        //   ],
+        // ),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+        color: Colors.cyan,
+        borderRadius: BorderRadius.circular(20),
+        gradient: LinearGradient(
+           begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Colors.cyan, 
+            Colors.orange
+            ],
+         
+        ),
+      ),
+      child: Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      Text(
+        "Rizky Adekatuasa",
+        style: TextStyle(
+          fontSize: 30,
+          fontWeight: FontWeight.bold,
+          color: Colors.white,
+        ),
+      ),
+      SizedBox(height: 10),
+      Text(
+        "Anak Teknik Informatika terbiasa dengan codingan.",
+        style: TextStyle(
+          fontStyle: FontStyle.italic,
+          color: Colors.white70,
+          fontSize: 14,
+        ),
+        textAlign: TextAlign.center,
+      ),
     ],
   ),
-  body: Container(
-        width: double.infinity,
-        height: 100,
-        color: Colors.blue,
-        alignment: Alignment.topCenter,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-          Icon(Icons.person),
-          SizedBox(width: 10),
-          Icon(Icons.account_balance),
-          SizedBox(width: 5),
-          Text("ini adalah data"),
-       ],)
-     ),  
+),
     );
   }
 }
